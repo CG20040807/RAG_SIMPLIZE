709c2982beff96a22ec8de6ee258629c9c606638
Cheng ge <3171785486@qq.com>
2026-04-14 15:12:15 +0800

    Update retriever.py

--- core/retriever.py
+++ core/retriever.py
@@ -1,6 +1,12 @@
 import faiss
 import pandas as pd
+import os
 from core.embedding import encode
+from core.index_builder import build
+
+# ❗如果index不存在 → 自动创建
+if not os.path.exists("vectorstore/faiss.index"):
+    build()
 
 index = faiss.read_index("vectorstore/faiss.index")
 df = pd.read_pickle("vectorstore/data.pkl")
