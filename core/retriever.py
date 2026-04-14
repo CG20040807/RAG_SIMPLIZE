d6054fa84002d3a74e278a2cbab1543e94c013d9
Cheng ge <3171785486@qq.com>
2026-04-14 14:08:26 +0800

    Update retriever.py

--- core/retriever.py
+++ core/retriever.py
@@ -1,12 +1,36 @@
 import faiss
 import numpy as np
 import pandas as pd
+import os
 from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-index = faiss.read_index("vectorstore/faiss.index")
-df = pd.read_pickle("vectorstore/meta.pkl")
+INDEX_PATH = "vectorstore/faiss.index"
+DATA_PATH = "vectorstore/meta.pkl"
+
+def build_if_not_exists():
+    if os.path.exists(INDEX_PATH):
+        return
+
+    os.makedirs("vectorstore", exist_ok=True)
+
+    df = pd.read_csv("data/products.csv")
+
+    texts = (df["name"] + " " + df["category"] + " " + df["description"]).tolist()
+    emb = model.encode(texts)
+
+    index = faiss.IndexFlatL2(emb.shape[1])
+    index.add(np.array(emb))
+
+    faiss.write_index(index, INDEX_PATH)
+    df.to_pickle(DATA_PATH)
+
+# 自动保证可用
+build_if_not_exists()
+
+index = faiss.read_index(INDEX_PATH)
+df = pd.read_pickle(DATA_PATH)
 
 def search(query, k=5):
     vec = model.encode([query])
