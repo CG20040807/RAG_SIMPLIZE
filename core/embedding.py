c5e4c521341c4f9ba29125e7dfcf192168f69169
Cheng ge <3171785486@qq.com>
2026-04-14 14:57:09 +0800

    Update embedding.py

--- core/embedding.py
+++ core/embedding.py
@@ -1,10 +1,6 @@
 from sentence_transformers import SentenceTransformer
-import pandas as pd
-import numpy as np
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-def get_embeddings(df):
-    texts = (df["name"] + " " + df["description"]).tolist()
-    embeddings = model.encode(texts, normalize_embeddings=True)
-    return np.array(embeddings)
+def encode(texts):
+    return model.encode(texts, normalize_embeddings=True)
