67db92a8a36ab67bfeca2d8f58925b9d47cdb2b9
Cheng ge <3171785486@qq.com>
2026-04-14 14:08:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import streamlit as st
 from core.rag import answer
+from core.retriever import build_if_not_exists
 
-st.set_page_config(page_title="电商RAG系统")
+st.title("🛒 电商RAG系统")
 
-st.title("🛒 电商AI推荐系统（RAG）")
+# 🔥 关键：启动时保证index存在
+build_if_not_exists()
 
-query = st.text_input("请输入你的需求（例如：200元跑鞋）")
+query = st.text_input("输入你的需求")
 
 if query:
-    with st.spinner("AI正在思考中..."):
-        result = answer(query)
-
-    st.markdown("### 推荐结果")
+    result = answer(query)
     st.write(result)
